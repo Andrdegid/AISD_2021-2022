7be4e4e9de4594f20badda026972d8c773e75693
Andrdegid <gusefff2003@yandex.ru>
2022-03-09 20:59:24 +0300

    first commit:)

---
+++ Source 1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	int a, b;
+	ifstream fin("aplusb.in");
+	ofstream fout("aplusb.out");
+	fin >> a >> b;
+	fout << a + b << endl;
+	return 0;
+}
